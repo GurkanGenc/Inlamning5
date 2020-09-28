d7e88705db4454ab5862ef7f428ea87da88821fc
Gürkan Genç <wishblood@gmail.com>
2020-09-28 20:47:46 +0200

    Add project files.

---
+++ DeviceApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceApp  // This is an IoT device
+{
+    class Program
+    {
+        private static DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=Inlamning3-iothub.azure-devices.net;DeviceId=ConsoleApp;SharedAccessKey=ptX988yL+vj8vS/Z4KQzewzdmmF4IJGyD2SLXCjCtgY=", TransportType.Mqtt);
+        private static int telemetryInterval = 5; // It is second.
+        private static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            deviceClient.SetMethodHandlerAsync("SetTelemetryInterval", SetTelemetryInterval, null).Wait();
+            SendMessageAsync().GetAwaiter();
+
+            Console.ReadKey();
+        }
+
+        // This function returns a MethodResponse and make that Task
+        // Since it is Task it does not need to be Async!
+        private static Task<MethodResponse> SetTelemetryInterval(MethodRequest request, object userContext)
+        {
+            var payload = Encoding.UTF8.GetString(request.Data).Replace("\"", "");
+
+            // TryParse means we test if the value is an int
+            if (Int32.TryParse(payload, out telemetryInterval))
+            {
+                Console.WriteLine($"Interval set to: {telemetryInterval} seconds.");
+
+                // We can write direct as a json. { "result": "Executed direct method: SetTelemetryInterval" }
+                string json = "{\"result\": \"Executed direct method: " + request.Name + "\"}";
+
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 200));
+            }
+            else
+            {
+                string json = "{\"result\": \"Method not implemented\"}";
+
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 501));
+            }
+        }
+
+        private static async Task SendMessageAsync()
+        {
+            while (true)
+            {
+                double temp = 10 + rnd.NextDouble() * 30;
+                double hum = 40 + rnd.NextDouble() * 40;
+
+                var data = new // "payload" is a variable that has a type as an object!
+                {
+                    temperature = temp,
+                    humidity = hum
+                };
+
+                var json = JsonConvert.SerializeObject(data);
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                // "Properties" here just reads string. P.g.a. "true and "false" values are strings.
+                payload.Properties.Add("temperatureAlert", (temp > 30) ? "true" : "false"); // condition ? true value : false value
+
+                await deviceClient.SendEventAsync(payload);
+                Console.WriteLine($"Message sent: {json}");
+
+                await Task.Delay(telemetryInterval * 1000);
+            }
+            
+            
+        }
+    }
+}
---
+++ ServiceApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceApp
+{
+    class Program
+    {
+        private static ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("HostName=Inlamning3-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=t6ZRawYie7F8lN3iQGm3jnmPac6NJRAWphc4+JRLAtg=");
+
+        static void Main(string[] args)
+        {
+            Task.Delay(5000).Wait(); // We don't need this. It is just to see what's going on.
+
+            InvokeMethod("ConsoleApp", "SetTelemetryInterval", "10").GetAwaiter();
+
+            Console.ReadKey();
+        }
+
+        // deviceId here is "ConsoleApp" and methodName is "SetTelemetryInterval".
+        // This method is same thing with "Call Method on Device" tab in the Device Explorer app.
+        static async Task InvokeMethod(string deviceId, string methodName, string payload)
+        {
+            var methodInvocation = new CloudToDeviceMethod(methodName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson(payload);
+
+            var response = await serviceClient.InvokeDeviceMethodAsync(deviceId, methodInvocation);
+            Console.WriteLine($"Response Status: {response.Status}");
+            Console.WriteLine(response.GetPayloadAsJson());
+        }
+    }
+}
